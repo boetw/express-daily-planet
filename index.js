00604f883b8de8b544dfae3a73a1420411b3a910
Boet Waldbjorn <boetw@me.com>
2016-08-09 09:01:43 -0700

    Site links work

---
+++ index.js
@@ -0,0 +1,54 @@
+//requires
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+var app = express();
+
+//set/use statements
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+//routes
+///SITE
+app.get("/", function(req, res) {
+	res.render('site/index');
+});
+app.get("/about", function(req, res) {
+	res.render('site/about');
+});
+app.get("/contact", function(req, res) {
+	res.render('site/contact');
+});
+///ARTICLES
+app.get("/articles", function(req, res) {
+	var fileContents = fs.readFileSync('./data.json');
+	var data = JSON.parse(fileContents);
+	res.render("articles", {
+		articles: data
+	});
+});
+app.get("/articles/new", function(req, res) {
+	res.render('articles/new');
+});
+app.get("/articles/:idx", function(req, res) {
+	var fileContents = fs.readFileSync('./data.json');
+	var data = JSON.parse(fileContents);
+	var articleIndex = parseInt(req.params.idx);
+	res.render("articles/show", {
+		articles: data[articleIndex]
+	});
+});
+////post
+app.post("/articles/new", function(req, res) {
+	var fileContents = fs.readFileSync('./data.json');
+	var data = JSON.parse(fileContents);
+	data.push(req.body);
+	fs.writeFileSync('./data.json', JSON.stringify(data));
+	res.redirect('/articles');
+});
+
+
+//listen
+app.listen(3000);
